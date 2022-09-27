383583fa875031e452e726084ad9f8c82ec9bd93
Zembik92 <kambouazembi@gmail.com>
2022-09-27 14:01:23 +0100

    second milestone edit

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -6,26 +6,43 @@ word_list = ["apple", "banana", "pineapple", "mango", "grapes"]
 word = random.choice(word_list) 
 print(word)
 
+# Task one, ask the user to input one letter
+while True:
+    guess = input("Enter a single letter: ")
+    if len(guess) ==1 and guess.isalpha():
+        break
+    else:
+        print("Invalid letter. Enter a single letter")
 
-def ask_for_input():
-    while True:   
-    #Using the input function, ask the user to input a letter
-        guess = (input("Enter a single letter: ")).lower()
-        def check_guess(guess):
-            if len(guess) == 1 and guess.isalpha():
-                if guess in word:
-                    print("Good job!!", guess, "is correct")
-                    
-                else:
-                    if guess not in word:
-                        print("Too Bad!!", guess, "is not correct. Try again")
-            else:
-                print("Sorry not valid input")
-        check_guess(guess)
-        break   
-ask_for_input()  
+#Task two, check if letter is in chosen word
+if guess in word:
+    print("Good job!!", guess, "is correct")           
+else:
+    if guess not in word:
+        print("Too Bad!!", guess, "is not correct. Try again")
+           
+
+#Task three, convert the guessed letter to lowercase and put code in fuction
+def check_guess(guess):
+    guess_lower = guess.lower()
+    if guess_lower in word:
+        print("Good job!!", guess_lower, "is correct")
+    else:
+        if guess_lower not in word:
+            print("Too Bad!!", guess_lower, "is not correct. Try again")  
                 
-    
+def ask_for_input():
+    while True:
+        guess = input("Enter a single letter: ")
+        if len(guess) ==1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Enter a single letter")
+    check_guess(guess)
+
+ask_for_input()
+      
+
     
 
            
